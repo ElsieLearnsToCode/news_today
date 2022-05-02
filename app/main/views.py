101d2ab29a7db490ad1d5ff0d97d941dcfc98503
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-05-02 12:08:51 +0300

    add a new route that has the sources() function

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
 
+
 # Views
 @app.route('/')
 def index():
@@ -10,3 +11,11 @@ def index():
     '''
     message = 'Welcome to News Today: Reliable, Timely and Objective'
     return render_template('index.html',message = message)
+
+@app.route('/articles/<id>')
+def sources(sources_id):
+
+    '''
+    View sources/articles function that returns the articles page and its data
+    '''
+    return render_template('sources.html',id = sources_id)
