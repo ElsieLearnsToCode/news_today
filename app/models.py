05022379103723762c4b6b06e7251a5f8f7b14f3
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-05-02 19:29:27 +0300

    create test classes for the Sources class

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ class Sources:
     Sources class to define Source Objects
     '''
 
-    def __init__(self,id,name,description,url,category,country,language):
+    def __init__(self,id,name,description,url,category,language,country):
         self.id = id
         self.name = name
         self.description = description
---
+++ tests/test_sources.py
@@ -0,0 +1,27 @@
+import unittest
+
+from app.models import Sources
+
+class SourcesTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Sources class
+    '''
+
+    def setUp(self):
+        self.new_source = Sources('abc-news',
+        'ABC News',
+        'Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.',
+        'https://abcnews.go.com','general','en',"us")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Sources))
+
+    
+    def test_to_verify_sources_variables(self):
+        self.assertEquals(self.new_source.id,'abc-news')
+        self.assertEquals(self.new_source.name,'ABC News')
+        self.assertEquals(self.new_source.description,'Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.')
+        self.assertEquals(self.new_source.url,'https://abcnews.go.com')
+        self.assertEquals(self.new_source.category,'general')
+        self.assertEquals(self.new_source.language,'en')
+        self.assertEquals(self.new_source.country,'us')
