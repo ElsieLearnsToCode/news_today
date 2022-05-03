bbb49fa2d76f1d246176eca6accfa33028019b30
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-05-03 15:20:38 +0300

    create a function that will process the results and create sources obejcts from the api

--- app/request.py
+++ app/request.py
@@ -20,7 +20,7 @@ def configure_request(app):
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_BASE_URL']
 
-def get_news_sources():
+def get_news_sources(category):
     '''
     Function that gets the json response to our url request
     '''
@@ -32,10 +32,35 @@ def get_news_sources():
 
         news_sources_results = None
 
-		if get_sources_response['sources']:
-			sources_results_list = get_sources_response['sources']
-			sources_results = process_sources(sources_results_list)
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_sources(sources_results_list)
 
-	return news_sources_results
+    return news_sources_results
 
+def process_sources(sources_list):
+    '''
+    Function that processes the news sources results and turns them into a list of objects
+    Args:
+        sources_list: A list of dictionaries that contain sources details
+    Returns:
+        sources_results: A list of sources objects
+    '''
+    sources_results = []
+
+    for source_item in sources_list:
+        id = source_item.get('id') 
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+        language = source_item.get('language')
+        country = source_item.get('country')
+
+
+        sources_object = Sources(id,name,description,url,category,country,language)
+        sources_results.append(sources_object)
+
+
+    return sources_results
 
