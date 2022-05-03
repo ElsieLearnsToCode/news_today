b1d966022ff01c4c9f0d50b3505a62ae91028a4c
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-05-03 18:40:59 +0300

    add markup to display the news sources

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from app import app
+from ..request import get_news_sources, get_news_articles
+from ..models import Sources, Articles
 
 
 # Views
@@ -9,9 +11,12 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    message = 'Welcome to News Today: Reliable, Timely and Objective'
+    business_sources = get_news_sources('business')
+    sports_sources = get_news_sources('sports')
+    technology_sources = get_news_sources('technology')
+    entertainment_sources = get_news_sources('entertainment')
     title = 'News Today'
-    return render_template('index.html', title = title, message = message)
+    return render_template('index.html', title = title, business_sources = business_sources,sports_sources = sports_sources,technology_sources = technology_sources,entertainment_sources = entertainment_sources)
 
 @app.route('/sources/<id>')
 def sources(id):
@@ -19,4 +24,7 @@ def sources(id):
     '''
     View sources/articles function that returns the sources page and its data
     '''
-    return render_template('sources.html',id = id)
+    articles = get_news_articles(id)
+    title = f'NH | {id}'
+
+    return render_template('sources.html',id = id, title= title,articles = articles)
