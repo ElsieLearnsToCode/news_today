df8395fd9e3d11ad194d40ad27c1153f8bb37322
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-05-03 13:52:11 +0300

    create the configure request function that takes in the application instance and replaces the values of the None variables to application configuration objects

--- app/request.py
+++ app/request.py
@@ -4,7 +4,22 @@ from app.models import Sources, Articles
 from datetime import datetime
 
 # Getting api key
-api_key = app.config['NEWS_API_KEY']
+# api_key = app.config['NEWS_API_KEY']
+api_key = None
+
+# Getting the news base url
+# base_url = app.config["NEWS_API_BASE_URL"]
+base_url = None
+
+#getting the articles url
+articles_url = None
+
+
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_API_BASE_URL']
+
 
 
 
